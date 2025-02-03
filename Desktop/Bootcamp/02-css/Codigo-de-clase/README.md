7a72ded9e2ee987f486d01c2190b02a7292551d7
Jero Medina V. <jeromedinav.@Mac-mini-de-Jero.local>
2025-02-03 18:18:43 +0100

    Eliminado Desktop/Bootcamp y otros archivos borrados del repositorio remoto

--- Desktop/Bootcamp/02-css/Codigo-de-clase/README.md
+++
@@ -1,1 +0,0 @@
-Códigos usados en clase
--- Desktop/Bootcamp/03-javascript/Codigo-de-clase/dia02/ejercicioCondicionales/script.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-// Crea un código que en base a una variable hora diga por consola "Buenos días", "buenas tardes" o "buenas noches", según corresponda
-
-let hora = 15;
